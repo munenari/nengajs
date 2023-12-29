b8c311b54a0b29d5aacfd99652957e0e72636976
munenari <munenari@gmail.com>
2023-12-29 20:48:18 +0900

    Fix layout

--- src/lib/Destination.svelte
+++ src/lib/Destination.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <section class="sheet">
-	<p class="郵便番号">{to.postcode}</p>
+	<p class="郵便番号">{to.postcode.replaceAll('-', '')}</p>
 	<p class="住所">{@html to.getNormAddress()}</p>
 	<div class="宛名">
 		{#each to.names as name}
@@ -15,9 +15,9 @@
 		<p class="差出人住所">{@html sender.getNormAddress()}</p>
 		<p class="差出人">
 			{#each sender.names as name}
-				<span>{@html name.replaceAll('_', ' ')}</span><br />
+				<div>{@html name.replaceAll('_', ' ')}</div>
 			{/each}
 		</p>
 	</div>
-	<p class="差出人郵便番号">{sender.postcode}</p>
+	<p class="差出人郵便番号">{sender.postcode.replaceAll('-', '')}</p>
 </section>
